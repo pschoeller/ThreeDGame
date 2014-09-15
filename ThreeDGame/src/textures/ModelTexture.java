14ea8dc659b8d9191c6ece36045072f30f97b95e
pschoeller <pschoeller001@gmail.com>
2014-09-14 20:04:19 -0500

    Initial commit

---
+++ ThreeDGame/src/textures/ModelTexture.java
@@ -0,0 +1,21 @@
+package textures;
+
+public class ModelTexture {
+	
+	private int textureID;
+	
+	
+	public ModelTexture(int id){
+		this.setTextureID(id);
+	}
+
+
+	public int getTextureID() {
+		return textureID;
+	}
+
+
+	public void setTextureID(int textureID) {
+		this.textureID = textureID;
+	}
+}
